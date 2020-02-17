e318d7433d5ddc836f4e5b2415c9ae3c18246da4
emerson-maranhao <emerson_maranhao@hotmail.com>
2020-02-17 16:47:03 -0400

    Criação de ProgressBar na tela de Buscar Dados do Veículo Criação de AlertDialog para status de vistoria Criação de adpater para o RecyclerView Correção de cor do Status da tela de Login

--- app/src/main/java/com/example/appvistoria/SearchDataActivity.kt
+++ app/src/main/java/com/example/appvistoria/SearchDataActivity.kt
@@ -1,22 +1,49 @@
 package com.example.appvistoria
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_search_data.*
+
 
 class SearchDataActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_data)
-        val btn_search: Button = findViewById(R.id.btn_search)
-        btn_search.setOnClickListener(View.OnClickListener { view: View? ->
-            val intent = Intent(this, AddSurveyActivity::class.java)
-            // start your next activity
+        //val btn_search: Button = findViewById(R.id.btn_search)
 
-            startActivity(intent)
-        })
+//        btn_search.setOnClickListener(View.OnClickListener { view: View? ->
+//            val intent = Intent(this, AddSurveyActivity::class.java)
+//            // start your next activity
+//
+//            startActivity(intent)
+//        })
+
+
+            btn_search.setOnClickListener {
+                if (progressBar != null) {
+                val visibility =
+                    if (progressBar.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+                progressBar.visibility = visibility
+                val handler = Handler()
+
+                handler.postDelayed({
+                    progressBar.visibility = View.GONE
+                    val intent = Intent(this, AddSurveyActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    startActivity(intent)
+
+
+                }, 1000)
+
+
+//                val btn_search = if (progressBar.visibility == View.GONE) "SHOW PROGRESSBAR" else "HIDE PROGRESSBAR"
+//                btn.text = btnText
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/appvistoria/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/appvistoria/ui/home/HomeFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appvistoria.R
+import com.example.appvistoria.adapter.SurveysAdapter
+import com.example.appvistoria.data.Survey
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
 
 class HomeFragment : Fragment() {
 
@@ -22,10 +28,103 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+
+
+        root.recyclerView?.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        root.recyclerView?.adapter = SurveysAdapter(getSurveys())
+
+
+//        with(recyclerView) {
+//            layoutManager = LinearLayoutManager(this@HomeFragment), RecyclerView.VERTICAL, false)
+//            setHasFixedSize(true)
+//            adapter = SurveysAdapter(getSurveys())
+//        }
+//        val emptyView: ImageView = root.findViewById(R.id.no_item_list)
+//        val list: ListView = root.findViewById(R.id.recyclerView)
+//        list.setEmptyView(emptyView)
+
+
         homeViewModel.text.observe(this, Observer {
-            textView.text = it
+            //textView.text = it
         })
         return root
+    }
+
+    fun getSurveys(): List<Survey> {
+        return listOf(
+            Survey(
+                "JXS0855",
+                "2007/2008",
+                "Fox",
+                "Automóvel",
+                "Vermelha",
+                "Passageiro",
+                "AM",
+                "9BWDAS53IOP005683",
+                "CCRAAFO165",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "10000",
+                "Parqueamento",
+                "Aprovada",
+                "2020-01-30"
+            ),
+            Survey(
+                "PHF3760",
+                "2014",
+                "FOD Ká",
+                "Automóvel",
+                "Branca",
+                "Passageiro",
+                "AM",
+                "9BWDAS53434OFRO44",
+                "CCRAAFO3FE5",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "10000",
+                "Detran-Sede",
+                "Pendente",
+                "2020-02-01"
+            ),
+            Survey(
+                "OAO8023",
+                "2005",
+                "Sandero",
+                "Automóvel",
+                "Cinza",
+                "Passageiro",
+                "AM",
+                "9BWDAS5398PR8753",
+                "CCRAAE44OIU",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "TESTE",
+                "",
+                "10000",
+                "São Francisco",
+                "Reprovada",
+                "2020-02-01"
+            )
+
+        )
     }
 }
